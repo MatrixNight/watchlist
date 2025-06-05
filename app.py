41397bce7b59195bb86f4c88be82ebdebf24b562
MatrixNight <cstsing235@gmail.com>
2025-06-05 22:45:27 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -63,11 +63,6 @@ def forge():
     db.session.commit()
     click.echo('Done!!!')
 
-@app.route('/')
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -77,3 +72,20 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
+    
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+    
